eb15becd6c8d8d76c2f0cd3f15c1d9289a061720
Aryn Harmon <arynh2@illinois.edu>
2018-03-30 11:57:30 -0500

    add the startup scripts

---
+++ init_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 8"
---
+++ init_script_small.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 4"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 4"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 4"
+gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py 4"
