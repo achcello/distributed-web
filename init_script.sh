698dba63b5e04fc37e2bdb7a38e9ba467b86f80e
Aryn Harmon <arynh2@illinois.edu>
2018-04-08 13:17:00 -0500

    get rid of existentialism

--- init_script.sh
+++ init_script.sh
@@ -7,5 +7,5 @@ while [ $counter -le $1 ]
 do
 	#echo here
 	((counter++))
-	gnome-terminal --geometry 50x25 --window-with-profile=sarte -e "python3 nodeCode.py $1"
+	gnome-terminal --geometry 50x25 -e "python3 nodeCode.py $1"
 done
